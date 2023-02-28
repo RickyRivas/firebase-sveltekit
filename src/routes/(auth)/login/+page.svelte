0db54d4db6282581c8af15178ac016aa096c7d7c
Ricky Rivas <rickyrivas918@gmail.com>
2023-02-27 22:27:56 -0600

    added more auth btns

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -22,7 +22,40 @@
 						<input type="password" name="password" placeholder="Password" />
 						<button class="btn">Login</button>
 					</form>
-					<p>Forgot your password? <a href="/">Click here</a></p>
+					<p class="divider"><span>Or</span></p>
+					<button id="login-google" class="login-btn">
+						<img
+							class=""
+							src="/google.svg"
+							alt=""
+							width="25"
+							height="25"
+							loading="lazy"
+							decoding="async"
+						/>Continue with Google</button
+					>
+					<button id="login-facebook" class="login-btn">
+						<img
+							class=""
+							src="/facebook.svg"
+							alt=""
+							width="25"
+							height="25"
+							loading="lazy"
+							decoding="async"
+						/>Continue with Facebook</button
+					>
+					<button id="login-apple" class="login-btn last">
+						<img
+							class=""
+							src="/apple.svg"
+							alt=""
+							width="25"
+							height="25"
+							loading="lazy"
+							decoding="async"
+						/>Continue with Apple</button
+					>
 				</div>
 			</div>
 		</div>
